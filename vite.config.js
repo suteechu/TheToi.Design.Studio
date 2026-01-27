359a0f9d0f50c987d64e308603c2597cd825c132
suteechu <96893367+suteechu@users.noreply.github.com>
2026-01-27 11:29:37 +0700

    Update App.jsx complete system

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/thetoi-design-studio/', // <--- บรรทัดนี้สำคัญที่สุด! ต้องมีเครื่องหมาย / ปิดท้ายด้วย
+  base: '/TheToi.Design.Studio/', // <--- บรรทัดนี้สำคัญที่สุด! ต้องมีเครื่องหมาย / ปิดท้ายด้วย
 })

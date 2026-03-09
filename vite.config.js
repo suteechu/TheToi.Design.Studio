1f8613b669f580457f2107297da182c2e47526d6
suteechu <96893367+suteechu@users.noreply.github.com>
2026-03-10 05:52:09 +0700

    Fix Infinite Loop and Chart bugs

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // ✅ แก้ตรงนี้เป็น './' ครับ (จุดและสแลช)
+  base: '/TheToi.Design.Studio/', // ✅ สำหรับ GitHub Pages ต้องมีบรรทัดนี้ครับ
 })
